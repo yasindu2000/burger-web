e182d7b3d7ce74492cb3f83687b1ef7326bf7632
yasindu2000 <yasindudahanayake@gmail.com>
2025-07-30 22:58:26 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,23 +7,23 @@ function Navbar() {
     
 
     <header className='fixed w-full  z-10 bg-tertiary shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] py-4 dark:bg-slate-900 dark:text-white'>
-        <div className='container flex flex-row justify-between items-center'>
+        <div className='container flex flex-row items-center justify-between'>
             <div>
-                <Link to='/' spy={true} smooth={true} duration={500}><h1 className='font-semibold text-2xl cursor-pointer text-secondary'>BurgerBurst</h1></Link>
+                <Link to='/' spy={true} smooth={true} duration={500}><h1 className='text-2xl font-semibold cursor-pointer text-secondary'>BurgerBurst</h1></Link>
             </div>
        
-        <nav className='hidden lg:flex gap-10 text-secondary font-semibold text-lg '>
-            <Link to='menu' spy={true} smooth={true} duration={500} className='cursor-pointer hover:text-primary transition duration-300 ease-in-out'>Menu</Link>
-            <Link to='deals' spy={true} smooth={true} duration={500} className='cursor-pointer hover:text-primary transition duration-300 ease-in-out'>Hot Deals</Link>
-            <Link to='special' spy={true} smooth={true} duration={500} className='cursor-pointer hover:text-primary transition duration-300 ease-in-out'>Special</Link>
-            <Link to='review' spy={true} smooth={true} duration={500} className='cursor-pointer hover:text-primary transition duration-300 ease-in-out'>Review</Link>
+        <nav className='hidden gap-10 text-lg font-semibold lg:flex text-secondary '>
+            <Link to='menu' spy={true} smooth={true} duration={500} className='transition duration-300 ease-in-out cursor-pointer hover:text-primary'>Menu</Link>
+            <Link to='deals' spy={true} smooth={true} duration={500} className='transition duration-300 ease-in-out cursor-pointer hover:text-primary'>Hot Deals</Link>
+            <Link to='special' spy={true} smooth={true} duration={500} className='transition duration-300 ease-in-out cursor-pointer hover:text-primary'>Special</Link>
+            <Link to='review' spy={true} smooth={true} duration={500} className='transition duration-300 ease-in-out cursor-pointer hover:text-primary'>Review</Link>
         </nav>
 
-        <div className='flex  items-center  gap-3'>
+        <div className='flex items-center gap-3'>
             <div>
                 <DarkMode/>
             </div>
-            <button className='bg-secondary py-2 px-4 text-white font-semibold rounded-lg hover:scale-105 transition duration-300 ease-in'>Order Now</button>
+            <button className='px-4 py-2 font-semibold text-white transition duration-300 ease-in rounded-lg bg-secondary hover:scale-105'>Order Now</button>
         </div>
         </div>
     </header>
--- src/layouts/DarkMode.js
+++ src/layouts/DarkMode.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import darkPng from "../assets/images/moon.png"
 import lightPng from "../assets/images/sun.png"
 
